650991b15f61a82db912949a265e1b35a53b42b7
Waleed ur Rehman <waleedrehman31@gmail.com>
2022-03-23 20:58:38 +0500

    setting up database

--- TODO APP/server/index.js
+++ TODO APP/server/index.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 // Export Routes
 const todoRoute = require("./routes/todo-route");
 
 // Express App Initialization
 const app = express();
+
 // Middleware
+dotenv.config();
 app.use(cors());
 app.use(express.json());
+
+// Setting up Database
+console.log(process.env.MONGO_URI);
+mongoose
+	.connect(process.env.MONGO_URI)
+	.then(console.log("Database Connected"))
+	.catch((e) => {
+		console.log(e);
+	});
 
 // API Routes
 app.use("/api/todo", todoRoute);
