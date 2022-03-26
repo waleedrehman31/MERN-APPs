8ae77b182d42e4c0f411001665a672d46ffbbe6c
Waleed ur Rehman <waleedrehman31@gmail.com>
2022-03-26 18:30:46 +0500

    Create React File structure

---
+++ TODO APP/client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./navbar.css";
+
+function Navbar() {
+	return (
+		<div>
+			<h1>MERN TODO APP</h1>
+			<ul>
+				<li>All TODO</li>
+				<li>New TODO</li>
+			</ul>
+		</div>
+	);
+}
+
+export default Navbar;
