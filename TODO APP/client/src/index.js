a214f07bca4526bddc79f58f32555a86ba27fb27
Waleed ur Rehman <waleedrehman31@gmail.com>
2022-03-27 16:29:20 +0500

    :sparkles: Deployment Setting :sparkles:

--- TODO APP/client/src/index.js
+++ TODO APP/client/src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<BrowserRouter basename="/">
+		<BrowserRouter basename="https://todo-app-three-rouge.vercel.app/">
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>,
