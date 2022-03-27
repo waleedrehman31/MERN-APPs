afee1f924e83c85598c278821037612d5b940fa8
Waleed ur Rehman <waleedrehman31@gmail.com>
2022-03-27 13:52:57 +0500

    :sparkles: Fetching data and Show it on page :sparkles:

--- TODO APP/client/src/components/todolist/Todos.jsx
+++ TODO APP/client/src/components/todolist/Todos.jsx
@@ -1,111 +1,100 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 // Import css
 import "./todo.css";
 
+// Base URl
+const baseURL = "http://localhost:5000/api/todos";
+
 function Todos() {
+	const [todos, setTodos] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get(baseURL)
+			.then((response) => {
+				setTodos(response.data);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}, [todos]);
+
+	if (!todos) {
+		return "no todo available right now";
+	}
+
+	const compelteTask = (id) => {
+		axios
+			.put(
+				`${baseURL}/${id}`,
+				{
+					isCompelte: true,
+				},
+				{
+					headers: {
+						"Content-Type": "application/json",
+					},
+				},
+			)
+			.then(() => {
+				alert("Todo Completed");
+			});
+	};
+
+	const deletePost = (id) => {
+		axios.delete(`${baseURL}/${id}`).then(() => {
+			alert("Post deleted!");
+		});
+	};
+
 	return (
 		<div className="body-background container">
 			<div className="card container">
 				<div className="card-title">All TODOs</div>
 				<table>
 					<thead>
-						<th>ID</th>
-						<th>TODO</th>
-						<th>STATUS</th>
-						<th>ACTION</th>
+						<tr>
+							<th>ID</th>
+							<th>TODO</th>
+							<th>STATUS</th>
+							<th>ACTION</th>
+						</tr>
 					</thead>
 					<tbody>
-						<tr>
-							<td>1</td>
-							<td>This is todo</td>
-							<td>Completed</td>
-							<td>
-								<button className="complete-button">Complete</button>
-								<button className="edit-button">Edit</button>
-								<button className="delete-button">Delete</button>
-							</td>
-						</tr>
-						<tr>
-							<td>1</td>
-							<td>This is todo</td>
-							<td>Completed</td>
-							<td>
-								<button className="complete-button">Complete</button>
-								<button className="edit-button">Edit</button>
-								<button className="delete-button">Delete</button>
-							</td>
-						</tr>
-						<tr>
-							<td>1</td>
-							<td>This is todo</td>
-							<td>Completed</td>
-							<td>
-								<button className="complete-button">Complete</button>
-								<button className="edit-button">Edit</button>
-								<button className="delete-button">Delete</button>
-							</td>
-						</tr>
-						<tr>
-							<td>1</td>
-							<td>This is todo</td>
-							<td>Completed</td>
-							<td>
-								<button className="complete-button">Complete</button>
-								<button className="edit-button">Edit</button>
-								<button className="delete-button">Delete</button>
-							</td>
-						</tr>
-						<tr>
-							<td>1</td>
-							<td>This is todo</td>
-							<td>Completed</td>
-							<td>
-								<button className="complete-button">Complete</button>
-								<button className="edit-button">Edit</button>
-								<button className="delete-button">Delete</button>
-							</td>
-						</tr>
-						<tr>
-							<td>1</td>
-							<td>This is todo</td>
-							<td>Completed</td>
-							<td>
-								<button className="complete-button">Complete</button>
-								<button className="edit-button">Edit</button>
-								<button className="delete-button">Delete</button>
-							</td>
-						</tr>
-						<tr>
-							<td>1</td>
-							<td>This is todo</td>
-							<td>Completed</td>
-							<td>
-								<button className="complete-button">Complete</button>
-								<button className="edit-button">Edit</button>
-								<button className="delete-button">Delete</button>
-							</td>
-						</tr>
-						<tr>
-							<td>1</td>
-							<td>This is todo</td>
-							<td>Completed</td>
-							<td>
-								<button className="complete-button">Complete</button>
-								<button className="edit-button">Edit</button>
-								<button className="delete-button">Delete</button>
-							</td>
-						</tr>
-						<tr>
-							<td>1</td>
-							<td>This is todo</td>
-							<td>Completed</td>
-							<td>
-								<button className="complete-button">Complete</button>
-								<button className="edit-button">Edit</button>
-								<button className="delete-button">Delete</button>
-							</td>
-						</tr>
+						{todos.map((todo) => {
+							return (
+								<tr key={todo._id}>
+									<td>{todo._id}</td>
+									<td>{todo.todo}</td>
+									<td>{todo.isCompelte ? "Completed" : "Pending"}</td>
+									<td>
+										<button
+											onClick={() => {
+												compelteTask(todo._id);
+											}}
+											className={
+												todo.isCompelte
+													? "done-button button"
+													: "complete-button button"
+											}
+										>
+											Complete
+										</button>
+										<button className="edit-button button">Edit</button>
+										<button
+											className="delete-button button"
+											onClick={() => {
+												deletePost(todo._id);
+											}}
+										>
+											Delete
+										</button>
+									</td>
+								</tr>
+							);
+						})}
 					</tbody>
 				</table>
 			</div>
