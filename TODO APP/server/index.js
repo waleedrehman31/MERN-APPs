9f393c0966d9c140697675479e84ce27c46fef34
Waleed ur Rehman <waleedrehman31@gmail.com>
2022-03-27 16:24:35 +0500

    :sparkles: Deployment Setting :sparkles:

--- TODO APP/server/index.js
+++ TODO APP/server/index.js
@@ -44,7 +44,7 @@ process.env.NODE_ENV = "production";
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "../", "client", "build")));
-	app.get("*", (req, res) => {
+	app.get("/*", (req, res) => {
 		res.sendFile(
 			path.resolve(__dirname, "../", "client", "build", "index.html"),
 		);
