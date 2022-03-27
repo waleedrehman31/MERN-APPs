5331da8ee0bfda7431c10899ed2d55534a61a0ab
Waleed ur Rehman <waleedrehman31@gmail.com>
2022-03-27 14:18:16 +0500

    :sparkles: Form Handling and adding new todo :sparkles:

--- TODO APP/client/src/components/todoform/TodoForm.jsx
+++ TODO APP/client/src/components/todoform/TodoForm.jsx
@@ -1,15 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
+//Import CSS
 import "./todoform.css";
 
+// Base URl
+const baseURL = "http://localhost:5000/api/todos";
+
 function TodoForm() {
+	const [value, setValue] = useState("");
+
+	function handleChange(event) {
+		setValue(event.target.value);
+	}
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		try {
+			axios
+				.post(
+					baseURL,
+					{
+						todo: value,
+					},
+					{
+						headers: {
+							"Content-Type": "application/json",
+						},
+					},
+				)
+				.then(() => {
+					alert("Todo Added SuccessFully");
+					setValue("");
+				});
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
 	return (
 		<div className="body-background container">
 			<div className="card container">
 				<div className="card-title">Enter Your TODO</div>
-				<form action="">
+				<form onSubmit={handleSubmit}>
 					<label htmlFor="todo">Todo</label>
-					<input type="text" name="todo" id="todo" />
-					<button className="add-button">ADD</button>
+					<input
+						type="text"
+						name="todo"
+						id="todo"
+						value={value}
+						onChange={handleChange}
+						required
+					/>
+					<input
+						className="add-button button"
+						type="submit"
+						name="submit"
+						id="submit"
+					/>
 				</form>
 			</div>
 		</div>
