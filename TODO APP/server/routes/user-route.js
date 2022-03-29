90ca20cd402dc1fa0611b98bd54228139a59033e
Waleed ur Rehman <waleedrehman31@gmail.com>
2022-03-29 11:07:03 +0500

    :sparkles: Hashing User Password :sparkles:

--- TODO APP/server/routes/user-route.js
+++ TODO APP/server/routes/user-route.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 
 const Routes = express.Router();
 
@@ -22,11 +23,15 @@ Routes.post("/register", async (req, res) => {
 		return res.status(400).send("Email Already Exist");
 	}
 
+	const salt = await bcrypt.genSalt(10);
+	const hashPassword = await bcrypt.hash(req.body.password, salt);
+
 	const user = new User({
 		name: req.body.name,
 		email: req.body.email,
-		password: req.body.password,
+		password: hashPassword,
 	});
+	
 	try {
 		const newUser = await user.save();
 		res.send(newUser);
